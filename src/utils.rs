5d50971d169f57e818651c165248b0774fb198d4
J-HowHuang <howardhuang2001@gmail.com>
2024-04-26 00:16:13 -0400

    mock catalog table path lookup

--- src/utils.rs
+++ src/utils.rs
@@ -1,40 +1,27 @@
 use istziio_client::client_api::{StorageClient, TableId};
 use istziio_client::storage_client::StorageClientImpl;
 use std::collections::HashMap;
-use std::fs;
-use std::path::Path;
 
 pub fn setup_client_1() -> Box<dyn StorageClient> {
     let server_url = std::env::var("SERVER_URL").unwrap_or(String::from("http://127.0.0.1:26379"));
     println!("server url: {}", &server_url);
-    let bench_files_path = "./bench_files".to_string();
-    let map = create_table_file_map(&bench_files_path).unwrap();
+    let map = create_table_file_map().unwrap();
     Box::new(StorageClientImpl::new_for_test(1, map.clone(), &server_url))
 }
 
 pub fn setup_client_2() -> Box<dyn StorageClient> {
     let server_url = std::env::var("SERVER_URL").unwrap_or(String::from("http://127.0.0.1:26379"));
     println!("server url: {}", &server_url);
-    let bench_files_path = "./bench_files".to_string();
-    let map = create_table_file_map(&bench_files_path).unwrap();
+    let map = create_table_file_map().unwrap();
     Box::new(StorageClientImpl::new_for_test(1, map.clone(), &server_url))
 }
 
-pub fn create_table_file_map(directory: &str) -> Result<HashMap<TableId, String>, std::io::Error> {
+pub fn create_table_file_map() -> Result<HashMap<TableId, String>, std::io::Error> {
     let mut table_file_map: HashMap<TableId, String> = HashMap::new();
-    let dir = Path::new(directory);
-
-    // Read the directory entries
-    let entries = fs::read_dir(dir)?;
 
     // Iterate over the entries
-    for (id, entry) in entries.enumerate() {
-        let entry = entry?;
-        if entry.path().is_file() {
-            // If the entry is a file, add it to the map with an incremental ID
-            let filename = entry.file_name().into_string().unwrap();
-            table_file_map.insert(id as TableId, filename);
-        }
+    for id in 0..5 {
+        table_file_map.insert(id as TableId, format!("1000000row_10col_{}.parquet", id));
     }
 
     Ok(table_file_map)
