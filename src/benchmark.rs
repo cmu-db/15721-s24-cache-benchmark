2760fcc7d97154b564a8654f470bc8e657d325dd
xx01cyx <caoyuanxin0531@outlook.com>
2024-04-29 21:10:59 +0000

    enable cache1 benchmark

--- src/benchmark.rs
+++ src/benchmark.rs
@@ -1,27 +1,25 @@
-use istziio_client::client_api::{StorageRequest, StorageClient};
-use std::path::PathBuf;
-use std::time::Instant;
-use tokio::sync::mpsc;
-use std::time::{Duration, SystemTime};
-use std::thread::sleep;
+use csv;
+use istziio_client::client_api::{StorageClient, StorageRequest};
 use std::collections::VecDeque;
 use std::error::Error;
-use csv;
+use std::path::PathBuf;
+use std::thread::sleep;
+use std::time::Instant;
+use std::time::{Duration, SystemTime};
+use tokio::sync::mpsc;
 
 // This scans the bench_files dir to figure out which test files are present,
 // then builds a map of TableId -> filename to init storage client(only when catalog is not available)
 // and also generates workload based on table ids. Finally it runs the workload
 
-
-
 pub struct TraceEntry {
     pub timestamp: u64,
-    pub request: StorageRequest
+    pub request: StorageRequest,
 }
 
 pub enum ClientType {
     Client1(),
-    Client2()
+    Client2(),
 }
 
 pub fn parse_trace(trace_path: PathBuf) -> Result<VecDeque<TraceEntry>, Box<dyn Error>> {
@@ -31,17 +29,25 @@ pub fn parse_trace(trace_path: PathBuf) -> Result<VecDeque<TraceEntry>, Box<dyn 
         // The iterator yields Result<StringRecord, Error>, so we check the
         // error here.
         let record = result?;
-        trace.push_back(TraceEntry{timestamp: record.get(0).unwrap().parse().unwrap(), request: StorageRequest::Table(record.get(1).unwrap().parse().unwrap())});
+        trace.push_back(TraceEntry {
+            timestamp: record.get(0).unwrap().parse().unwrap(),
+            request: StorageRequest::Table(record.get(1).unwrap().parse().unwrap()),
+        });
     }
     Ok(trace)
 }
-pub async fn run_trace(mut trace: VecDeque<TraceEntry>, client_builder: &dyn Fn() -> Box<dyn StorageClient>) {
+pub async fn run_trace(
+    mut trace: VecDeque<TraceEntry>,
+    client_builder: &dyn Fn() -> Box<dyn StorageClient>,
+) {
     let start_time = SystemTime::now();
     let request_num = trace.len();
     let (tx, mut rx) = mpsc::channel(32);
     while !trace.is_empty() {
         let next_entry = trace.pop_front().unwrap();
-        if let Some(diff) = Duration::from_millis(next_entry.timestamp).checked_sub(start_time.elapsed().unwrap()) {
+        if let Some(diff) =
+            Duration::from_millis(next_entry.timestamp).checked_sub(start_time.elapsed().unwrap())
+        {
             sleep(diff);
         }
         println!("next trace: {}", next_entry.timestamp);
@@ -56,7 +62,7 @@ pub async fn run_trace(mut trace: VecDeque<TraceEntry>, client_builder: &dyn Fn(
             let client_start = Instant::now();
             let req = next_entry.request;
 
-            let res = client.request_data_sync(req.clone()).await;
+            let res = client.request_data(req.clone()).await;
             if let Err(e) = res {
                 println!("Error: {}", e);
             }
