a0f5ea1b1638967931f50aeec7f054e83d4862cb
xx01cyx <caoyuanxin0531@outlook.com>
2024-05-01 20:20:43 +0000

    enable log

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,10 @@ use std::path::PathBuf;
 async fn main() {
     // benchmark_sync().await;
     // benchmark_parallel().await;
+    let _ = env_logger::builder()
+        .filter_level(log::LevelFilter::Info)
+        .try_init();
+
     let trace = parse_trace(PathBuf::from("traces/trace_1m.csv")).unwrap();
     run_trace(trace, &utils::setup_client_1).await;
     let trace = parse_trace(PathBuf::from("traces/trace_100m.csv")).unwrap();
