01bfb7ec2194c772b07bf2e1ab63407383582cc6
Lan Lou <lanlou1554@gmail.com>
2024-04-30 21:50:34 -0400

    add 1m parallel trace file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use client_benchmark::{benchmark::{parse_trace, run_trace}, utils};
+use client_benchmark::{
+    benchmark::{parse_trace, run_trace},
+    utils,
+};
 use std::path::PathBuf;
 
 #[tokio::main]
 async fn main() {
     // benchmark_sync().await;
     // benchmark_parallel().await;
-    let trace = parse_trace(PathBuf::from("traces/trace_100m.csv")).unwrap();
+    let trace = parse_trace(PathBuf::from("traces/trace_1m_parallel.csv")).unwrap();
     run_trace(trace, &utils::setup_client_1).await;
 }
