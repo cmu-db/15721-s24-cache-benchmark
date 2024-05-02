697f0f0785f7e7c89172f5b018181d73ab62790d
xx01cyx <caoyuanxin0531@outlook.com>
2024-05-02 13:23:25 +0000

    9 caches run serial

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,8 @@ async fn main() {
 
     // let trace = parse_trace(PathBuf::from("traces/trace_1m.csv")).unwrap();
     // let trace = parse_trace(PathBuf::from("traces/trace_100m.csv")).unwrap();
-    let trace = parse_trace(PathBuf::from("traces/trace_parallel.csv")).unwrap();
-    // let trace = parse_trace(PathBuf::from("traces/trace_serial.csv")).unwrap();
+    // let trace = parse_trace(PathBuf::from("traces/trace_parallel.csv")).unwrap();
+    let trace = parse_trace(PathBuf::from("traces/trace_serial.csv")).unwrap();
     // let trace = parse_trace(PathBuf::from("traces/trace_repeat.csv")).unwrap();
     run_trace(trace, &utils::setup_client_1).await;
 }
